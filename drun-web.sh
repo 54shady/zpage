0805338e1fbec3cc2b0d21b7da81a9c517a33310
zeroway <M_O_Bz@163.com>
2025-11-13 11:41:32 +0800

    Setup pass in script, encrypt zdoc page
    config the password in run script
    encrypt the zdoc page
    add entry for pages
    
    the password file now in the container
    
        docker exec -it web cat /tmp/htpasswd
        zero:eBhvkVZiwEiqg

--- drun-web.sh
+++ drun-web.sh
@@ -1,8 +1,11 @@
-docker run -d --restart always --name web -p 8080:80 \
+docker run -d --restart always --name web -p 80:80 \
 	-v ${HOME}/github/myx/zpage:/usr/share/nginx/html \
 	-v ${HOME}/github/myx/zpage/default.conf:/etc/nginx/conf.d/default.conf \
-	-v ${HOME}/github/myx/zpage/passwd:/tmp/htpasswd \
 	-v ${HOME}/github/myx/zdoc/_build/html:/zdoc \
 	-v ${HOME}/github/myx/zdoc/_build/html/_static:/script \
+	-v ${HOME}/Videos:/video \
+	-v /data/p4/pdf:/pdf \
 	-v ${HOME}/github/myx/res:/res \
-	nginx
+	-v ${HOME}/github/myx/page:/page \
+	nginx \
+		sh -c "echo -n 'zero:' > /tmp/htpasswd && echo '0' | openssl passwd -stdin >> /tmp/htpasswd && nginx -g 'daemon off;'"
