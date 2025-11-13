19c20e4522970d962349eb40c2924811509aac4a
zeroway <M_O_Bz@163.com>
2025-11-13 15:43:19 +0800

    Abandon the res directory

--- drun-web.sh
+++ drun-web.sh
@@ -5,7 +5,6 @@ docker run -d --restart always --name web -p 80:80 \
 	-v ${HOME}/github/myx/zdoc/_build/html/_static:/script \
 	-v ${HOME}/Videos:/video \
 	-v /data/p4/pdf:/pdf \
-	-v ${HOME}/github/myx/res:/res \
 	-v ${HOME}/github/myx/page:/page \
 	nginx \
 		sh -c "echo -n 'zero:' > /tmp/htpasswd && echo '0' | openssl passwd -stdin >> /tmp/htpasswd && nginx -g 'daemon off;'"
